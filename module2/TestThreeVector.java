f91cdc24d16bfc355f785b72bdfaf310c93166bc
Patrick Chin <p.ch@live.co.uk>
2016-10-12 12:36:12 +0100

    added ThreeVector and TestThreeVector

---
+++ module2/TestThreeVector.java
@@ -0,0 +1,42 @@
+package module2;
+
+import module2.ThreeVector;
+
+public class TestThreeVector {
+
+	public static void main(String[] args) {
+		ThreeVector v1 = new ThreeVector(5,2,3);
+		ThreeVector v2 = new ThreeVector(4,5,1);
+		ThreeVector v3 = new ThreeVector(0,0,0);
+
+		System.out.println("Initial ThreeVectors: ");
+		System.out.println("\tv1: "+v1);
+		System.out.println("\tv2: "+v2);
+		System.out.println("\tv3: "+v3);
+		System.out.println();
+
+		System.out.println("Scalar products (static): ");
+		System.out.println("\tv1.v2: "+ThreeVector.scalarProduct(v1, v2));
+		System.out.println("\tv1.v3: "+ThreeVector.scalarProduct(v1, v3));
+		System.out.println();
+
+		System.out.println("Scalar products (non-static): ");
+		System.out.println("\tv1.v2: "+v1.scalarProduct(v2));
+		System.out.println("\tv1.v3: "+v1.scalarProduct(v3));
+		System.out.println();
+
+		System.out.println("Angle between vectors (static): ");
+		System.out.println("\tv1.v2: "+ThreeVector.angle(v1, v2));
+		System.out.println("\tv1.v3: "+ThreeVector.angle(v1, v3));
+		System.out.println();
+
+		System.out.println("Angle between vectors (non-static): ");
+		System.out.println("\tv1.v2: "+v1.angle(v2));
+		System.out.println("\tv1.v3: "+v1.angle(v3));
+		System.out.println();
+
+		System.out.println(v1);
+		System.out.println("Without a toString method, printing an object will"+
+				"print the class name of the object and it's hash code.");
+	}
+}
