ae0a6ab3f4b6eadcc7056586ed901cb0bc36cefd
Patrick Chin <p.ch@live.co.uk>
2016-11-11 16:08:57 +0000

    complete SquareMatrix class

--- src/module5/SquareMatrix.java
+++ src/module5/SquareMatrix.java
@@ -2,6 +2,10 @@ package module5;
 
 import java.util.Arrays;
 
+/**
+ * Class representing a matrix of dimensions n by n, which provides functions 
+ * including add, subtract and multiply.
+ */
 public class SquareMatrix {
 
 	private int size;
@@ -12,15 +16,8 @@ public class SquareMatrix {
 	 */
 	private SquareMatrix() { }
 
-	public SquareMatrix(int sz) {
-		if (sz < 0) {
-			throw new IllegalArgumentException("Cannot have a matrix of zise less than zero.");
-		}
-		size = sz;
-		data = new double[size][size];
-	}
-
 	/**
+	 * Initalize SquareMatrix using a deep copy of elems.
 	 */
 	public SquareMatrix(double[][] elems) {
 		this.fill(elems);
@@ -31,16 +28,18 @@ public class SquareMatrix {
 	 * This may expose this.data
 	 */
 	public void assign(double[][] elems) {
-		this.size = getSize(elems);
-		this.data = data;
+		this.size = calcSize(elems);
+		this.data = elems;
 	}
 
 	/**
 	 * Deep copies the argument data to this.data
 	 */
 	public void fill(double[][] elems) {
-		this.size = getSize(elems);
+		this.size = calcSize(elems);
+		// Initalise zero matrix
 		this.data = new double[size][size];
+		// Copy each element
 		for (int i = 0; i < size; i++) {
 			for (int j = 0; j < size; j++) {
 				this.data[i][j] = elems[i][j];
@@ -48,16 +47,29 @@ public class SquareMatrix {
 		}
 	}
 
-	public double getAt(int i, int j) {
+	/**
+	 * Returns the element M_ij where i is the row number and j is the column
+	 * number.
+	 */
+	public double get(int i, int j) {
 		return this.data[i][j];
 	}
 
-	private static int getSize(double[][] data) {
+	/**
+	 * Returns the length of an array of an array.
+	 * Throws if the length of data is less than one or if the length of data
+	 * is not equal to the length of the first element of data.
+	 */
+	private static int calcSize(double[][] data) {
 		if (data.length < 1) {
-			throw new IllegalArgumentException("Arguments 'elems' must have a length greater than zero.");
+			throw new IllegalArgumentException(
+				"Arguments 'elems' must have a length greater than zero."
+			);
 		}
 		if (data.length != data[0].length) {
-			throw new IllegalArgumentException("Argument 'elems' must have equal 'width' and 'height'.");
+			throw new IllegalArgumentException(
+				"Argument 'elems' must have equal 'width' and 'height'."
+			);
 		}
 		return data.length;
 	}
@@ -66,7 +78,7 @@ public class SquareMatrix {
 	 * Create the identity matrix with dimensions size x size.
 	 * Throws if size < 0.
 	 */
-	public SquareMatrix unitMatrix(int size) {
+	public static SquareMatrix unitMatrix(int size) {
 		if (size < 0) {
 			throw new IllegalArgumentException("Cannot have a matrix size less than zero.");
 		}
@@ -80,7 +92,7 @@ public class SquareMatrix {
 
 		return new SquareMatrix(mat);
 	}
-	
+
 	@Override
 	public int hashCode() {
 		final int prime = 31;
@@ -106,42 +118,118 @@ public class SquareMatrix {
 		return true;
 	}
 
+	/**
+	 * Returns the resultant matrix of an elementwise addittion between SquareMatrix this and SquareMatrix m
+	 */
+	public SquareMatrix add(SquareMatrix m) {
+		return add(this, m);
+	}
+
+	/**
+	 * Returns the resultant matrix of an elementwise addittion between SquareMatrix m1 and SquareMatrix m2
+	 */
 	public static SquareMatrix add(SquareMatrix m1, SquareMatrix m2) {
 		if (m1.size != m2.size) {
 			throw new IllegalArgumentException("Size of matrixies m1 and m2 must be equal.");
 		}
 
 		int size = m1.size;
-
+		// Zero initalise array
 		double[][] data = new double[size][size];
 		for (int i = 0; i < size; i++) {
 			for (int j = 0; j < size; j++) {
-				data[i][j] = m1.data[i][j] + m2.data[i][j]
+				data[i][j] = m1.data[i][j] + m2.data[i][j];
 			}
 		}
 
 		// Create a SquareMatrix without initalising it's members
 		SquareMatrix m = new SquareMatrix();
+		// Shallow copy of data as a deep copy is not needed here
 		m.assign(data);
-
 		return m;
 	}
 
-	public static void main(String[] args) {
-		SquareMatrix m1 = new SquareMatrix(4);
+	/**
+	 * Returns the resultant matrix of an elementwise subtraction between SquareMatrix this and SquareMatrix m
+	 */
+	public SquareMatrix subtract(SquareMatrix m) {
+		return subtract(this, m);
+	}
 
-		{
-			double[][] data = {{1,1,1,1},
-							   {1,1,1,1},
-							   {1,1,1,1},
-							   {1,1,1,1}};
-			m1.assign(data);
-
-			data[0][0] = 0;
+	/**
+	 * Returns the resultant matrix of an elementwise subtraction between SquareMatrix m1 and SquareMatrix m2
+	 */
+	public static SquareMatrix subtract(SquareMatrix m1, SquareMatrix m2) {
+		if (m1.size != m2.size) {
+			throw new IllegalArgumentException("Size of square matrixies m1 and m2 must be equal.");
 		}
 
-		System.out.println(m1.data[0][0]);
+		int size = m1.size;
+		// Zero initalise array
+		double[][] data = new double[size][size];
+		for (int i = 0; i < size; i++) {
+			for (int j = 0; j < size; j++) {
+				data[i][j] = m1.data[i][j] - m2.data[i][j];
+			}
+		}
 
+		// Create a SquareMatrix without initalising it's members
+		SquareMatrix m = new SquareMatrix();
+		// Shallow copy of data as a deep copy is not needed here
+		m.assign(data);
+		return m;
 	}
+
+	/**
+	 * Returns the resultant matrix of an inner product between SquareMatrix this and SquareMatrix m
+	 */
+	public SquareMatrix multiply(SquareMatrix m) {
+		return multiply(this, m);
+	}
+
+	/**
+	 * Returns the resultant matrix of an inner product between SquareMatrix m1 and SquareMatrix m2
+	 */
+	public static SquareMatrix multiply(SquareMatrix m1, SquareMatrix m2) {
+		if (m1.size != m2.size) {
+			throw new IllegalArgumentException("Size of square matrixies m1 and m2 must be equal.");
+		}
+
+		int size = m1.size;
+
+		// zero initalise data of resultant matrix
+		double[][] data = new double[size][size];
+
+		// for each element in the resultant matrix
+		for (int i = 0; i < size; i++) {
+			for (int j = 0; j < size; j++) {
+
+				// multiply row i of m1 by column of column j of m2 and add to sum
+				for (int k = 0; k < size; k++) {
+					data[i][j] += m1.data[i][k] * m2.data[k][j];
+				}
+			}
+		}
+
+		// Create a SquareMatrix without initalising it's members
+		SquareMatrix m = new SquareMatrix();
+		// Shallow copy of data as a deep copy is not needed here
+		m.assign(data);
+		return m;
+	}
+
+	public String toString() {
+		StringBuilder sb = new StringBuilder();
+		for (int i = 0; i < this.size; i++) {
+			sb.append("[");
+			for (int j = 0; j < this.size; j++) {
+				if (j != 0) { sb.append(", "); }
+				sb.append(this.data[i][j]);
+			}
+			sb.append("]\n");
+		}
+		return sb.toString();
+	}
+
 
 }
